eb7c8010a87c56fec1f2304e95ced9d0023241c7
mike <mikeover719@gmail.com>
2015-12-15 06:04:47 -0800

    Move angular into bower, add toaster

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ var app = angular.module('codecraft', [
   'angularSpinner',
   'jcs-autoValidate',
   'angular-ladda',
-  'mgcrea.ngStrap'
+  'mgcrea.ngStrap',
+  'toaster',
+  'ngAnimate'
 ]);
 
 app.config(
@@ -86,7 +88,7 @@ app.controller('PersonListController', ['$scope', '$modal', 'ContactService', fu
     
 }]);
 
-app.service('ContactService', function(Contact, $q) {
+app.service('ContactService', function(Contact, $q, toaster) {
   
   var self = {
     'addPerson': function(person) {
@@ -148,8 +150,9 @@ app.service('ContactService', function(Contact, $q) {
     'updateContact': function(person) {
       console.log('updateContact'); 
       self.isSaving = true;
-      person.$update.then(function() {
+      person.$update().then(function() {
         self.isSaving = false;
+        toaster.pop('success', 'Updated ' + person.name);
       });
     },
     'removeContact': function(person) {
@@ -160,6 +163,7 @@ app.service('ContactService', function(Contact, $q) {
         var index = self.persons.indexOf(person);
         self.persons.splice(index, 1);
         self.selectedPerson = null;
+        toaster.pop('success', 'Deleted ' + person.name);
       });
     },
     'createContact': function(person) {
@@ -173,6 +177,7 @@ app.service('ContactService', function(Contact, $q) {
         self.page = 1;
         self.persons = [];
         self.loadContacts();
+        toaster.pop('success', 'Created ' + person.name);
         defer.resolve();
       });
       return defer.promise;
