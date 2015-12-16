36cf475f81da2841f5c44b35574b8e42eeb3e154
mike <mikeover719@gmail.com>
2015-12-16 06:54:18 -0800

    Remove the controller from the list.html template and pass it in from ui-router as a parameter PersonListController

--- main.js
+++ main.js
@@ -14,7 +14,8 @@ app.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('list', {
       url: "/",
-      templateUrl: 'templates/list.html'
+      templateUrl: 'templates/list.html',
+      controller: 'PersonListController'
     });
   
   $urlRouterProvider.otherwise('/');
